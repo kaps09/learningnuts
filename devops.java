7752a38283263f53ae23a440a5fb6cc8c3482c82
Kapil Pandit <kapilp.pandit@gmail.com>
2021-03-26 17:06:30 +0530

    first

--- devops.java
+++ devops.java
@@ -1,1 +1,8 @@
-public class devops{	public static void main( String[] args){	       for (int i=1; i<=10; i++){	       	       System.out.println("Devops initial Program"+i);		       }	}	}	
+public class devops{
+	public static void main(String[] args){
+		for (int i=1; i<=10; i++){
+			System.out.println("DevOps is hot topic these days "+i);
+		}
+	}
+
+}
