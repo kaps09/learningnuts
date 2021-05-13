4a44fbb0b05da36ebf6824e38405fc83cad62ded
Kapil Pandit <kapilp.pandit@gmail.com>
2021-05-13 20:18:17 +0530

    Initial Phase of testing

---
+++ src/main/java/project7/project7.java
@@ -0,0 +1,10 @@
+package project7;
+
+public class project7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/project7/project7test.java
@@ -0,0 +1,14 @@
+package project7;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class project7test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
